9ecfd39d4d9414e91e1e0f1433c0d8761b43a711
Josiah Keller <josiahbkeller@gmail.com>
2021-01-26 22:49:35 -0500

    Add styling for board cleared screen

--- src/components/BoardClearedScreen.vue
+++ src/components/BoardClearedScreen.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="board-cleared">
-    Board cleared!
-    <table>
-      <tr>
-        <th scope="row">Boards Cleared</th>
-        <td>{{ boardsCleared }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Time Spent Playing</th>
-        <td>{{ timeAccumulator | gameTime }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Longest Run Without Discard</th>
-        <td>{{ maxRun }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Current Score</th>
-        <td>{{ score }}</td>
-      </tr>
-    </table>
-    <button @click="nextLevel()">Play next board</button>
-    <button @click="returnToMainMenu()">Main Menu</button>
+    <div class="board-cleared-wrapper">
+      <h1 class="board-cleared-title">Board cleared</h1>
+      <table class="board-cleared-stats">
+        <tr>
+          <th scope="row">Boards Cleared</th>
+          <td>{{ boardsCleared }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Time Spent Playing</th>
+          <td>{{ timeAccumulator | gameTime }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Longest Run Without Discard</th>
+          <td>{{ maxRun }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Current Score</th>
+          <td>{{ score }}</td>
+        </tr>
+      </table>
+      <button @click="nextLevel()" v-sparkle="{ maxParticles: 6, minInterval: 100, maxInterval: 300 }">Play next board</button>
+      <button @click="returnToMainMenu()">Main Menu</button>
+    </div>
   </div>
 </template>
 
@@ -61,13 +63,47 @@ export default {
 </script>
 
 <style lang="scss">
+  @import "@/global.scss";
+
   .board-cleared {
-    position: absolute;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    background: #999;
+    @include backdrop-container;
     z-index: 100;
+    font-family: "Fraunces", "Times New Roman", serif;
+    user-select: none;
+
+    .board-cleared-wrapper {
+      @include menu-wrapper;
+
+      .board-cleared-title {
+        font-size: 52ps;
+        color: rgb(21, 37, 19);
+        text-transform: uppercase;
+        margin-bottom: 50px;
+      }
+
+      .board-cleared-stats {
+        width: 80%;
+        margin: auto;
+        text-align: left;
+        font-size: 20px;
+        margin-bottom: 50px;
+
+        td {
+          padding: 2px 0px;
+        }
+      }
+
+      button {
+        @include gloss-button-base;
+        border-color: rgb(0, 100, 0);
+        background-color: rgb(0, 150, 0);
+        background-image: linear-gradient(to top, rgb(0, 100, 0) 0%, rgb(0, 90, 0) 30%, rgb(0, 80, 0) 49.9999%, rgb(0, 120, 0) 50%, rgb(0, 140, 0) 100%);
+        transition: box-shadow 0.1s linear;
+
+        &:hover {
+          box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5) inset, 0px 0px 10px rgba(0, 0, 0, 0.25), 0px 0px 30px rgb(0, 140, 0);
+        }
+      }
+    }
   }
 </style>
