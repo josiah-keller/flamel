76be7652e5cb2f150014d0a335dfe682ac943f4f
Josiah Keller <josiahbkeller@gmail.com>
2021-01-30 17:31:00 -0500

    More styling for play area, forge

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -17,13 +17,20 @@
         <span class="level-value">{{ level }}</span>
       </div>
     </div>
-      
     <div class="nav-buttons">
       <button @click="returnToMainMenu()">New Game</button>
     </div>
-    <p>Forge: {{ forge }} / {{ maxForges }}</p>
-    <p><button @click="discard">Discard</button></p>
-    <p><button @click="dumpState">Dump State JSON to Console</button></p>
+    <div class="forge">
+      <div class="forge-wrapper">
+        <div class="forge-fill" :style="{ height: `${(100 * forge / maxForges).toFixed(2)}%` }"></div>
+      </div>
+      <div class="forge-discard">
+        <button @click="discard">Discard</button>
+      </div>
+    </div>
+    <div class="debug" v-if="debugEnabled">
+      <button @click="dumpState">Dump State JSON to Console</button>
+    </div>
   </div>
 </template>
 
@@ -48,10 +55,17 @@ export default {
       console.log(JSON.stringify(store.state));
     },
   },
+  data() {
+    return {
+      debugEnabled: false,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
+  @import "@/global.scss";
+
   .status-bar {
     flex: 0 0 300px;
     background: #898677;
@@ -121,7 +135,6 @@ export default {
         font-size: 16px;
         color: #a3a08f;
         padding: 5px 15px;
-        margin-bottom: 2px;
         border: 1px solid #898677;
         border-radius: 3px;
         background: #494638;
@@ -135,8 +148,53 @@ export default {
         &:active {
           background: #3d3b2f;
           box-shadow: 0px 2px #1f1d18 inset;
-          margin-top: 2px;
-          margin-bottom: 0px;
+        }
+      }
+    }
+
+    .forge {
+      .forge-wrapper {
+        border: 12px solid;
+        border-radius: 3px;
+        border-image: url("../assets/wrapper-border.png") 25 / 12px round;
+        box-shadow: 0px 1px 2px 1px rgba(0, 0, 0, 0.4), 0px 0px 10px rgba(0, 0, 0, 0.5) inset;
+        background: #000;
+        background-image: linear-gradient(to top, #292720 0%, #1d1b17 100%);
+        margin: 20px 60px 0px 60px;
+        height: 100px;
+        position: relative;
+      }
+      .forge-fill {
+        position: absolute;
+        left: 0%;
+        bottom: 0%;
+        width: 100%;
+        height: 0%;
+        background-color: #aa0000;
+        background-image: url("../assets/forge-noise.png"), url("../assets/forge-noise.png"),
+          linear-gradient(to top, #aa0000 0%, #ad0000 90%, #ad5500 100%);
+        background-size: 600px, 1200px;
+        animation: liquid-texture 40s linear infinite;
+        box-shadow: 0px 0px 20px #aa0000;
+        transition: height 0.5s ease-out;
+      }
+      .forge-discard {
+        text-align: center;
+
+        button {
+          padding: 5px 30px;
+          background: #343030;
+          border: 0px;
+          border-bottom-left-radius: 5px;
+          border-bottom-right-radius: 5px;
+          color: rgba(255, 255, 255, 0.6);
+          font-family: "Fraunces", "Times New Roman", serif;
+          text-transform: uppercase;
+          cursor: pointer;
+
+          &:hover {
+            background: #413c3c;
+          }
         }
       }
     }
